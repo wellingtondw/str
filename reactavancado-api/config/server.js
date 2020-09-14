3242254ba2c7421e92fbbc49394d9eec6755bcbe
wellington <wellingtonslpbi@hotmail.com>
2020-09-14 09:09:22 -0300

    first commit

---
+++ reactavancado-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'faf16bece3e440392f1c0ff92895b142'),
+    },
+  },
+});
